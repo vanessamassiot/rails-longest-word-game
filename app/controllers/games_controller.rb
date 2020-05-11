81fa639b474833b3efa6b18c8df1346b7f427c94
vanessa massiot <vanessa.massiot@free.fr>
2020-05-11 13:37:03 +0200

    display letters

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,10 @@
 class GamesController < ApplicationController
 	def new
+		source = ('A'..'Z').to_a
+		@letters = ""
+		10.times{ @letters += source[rand(source.size)].to_s}
+		return @letters
+
 	end
 
 	def score
